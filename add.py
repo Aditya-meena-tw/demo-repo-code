0812af8cbd68be8b272f15bb13812a603ae5a417
aditya meena <adityameena@thoughtwin.com>
2023-05-02 12:00:20 +0530

    wrong commit

--- add.py
+++ add.py
@@ -7,3 +7,11 @@ a1 = 5
 b1 = 3
 c1 = a1 - b1
 print(c1)#cheching the rebase
+print(c1) #this is to check conflicts
+print(c1)# it will print subracted value
+
+a2 = 34
+b2 = 4
+c2 = a2*b2
+print(c2)
+# this is new upldate in code to check the revert command
